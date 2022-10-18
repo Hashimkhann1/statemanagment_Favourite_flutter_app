6aeee025b4f6528555becaccecc6348865929634
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2022-10-18 23:41:21 +0500

    Project setup

---
+++ lib/view/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Text('Home screen')
+          ],
+        ),
+      ),
+    );
+  }
+}
